7b7b86ba0a2fe4e7df170907e1a8ac37542a12f1
Dan Garro <dobetterwithdan@gmail.com>
2022-08-14 19:33:29 -0400

    some testing to add new features

--- test.py
+++ test.py
@@ -1,17 +1,27 @@
 # Testing storing data a certain way for app:
 import json
 import re
+from pathlib import Path
+
 
 start_date = "2022-08-10"
 end_date = "2022-08-14"
 
-with open("ec_app_data/user_settings.json", "r") as file:
-    jsonData = json.load(file)
-print(jsonData["user_settings"]["user_email"])
-print(jsonData["user_settings"]["sales_tax"])
+y_start = start_date[:4]
+m_start = start_date[5:7]
+d_start = start_date[8:]
+y_end = end_date[:4]
+m_end = end_date[5:7]
+d_end = end_date[8:]
+date_search = range(int(d_start), int(d_end) + 1)
+re.findall("2022-08-")
+dir = r"C:/Users/16092/OneDrive/Desktop/practice_kivy/ec_expenses"
+file = Path(dir).glob("*")
+matches = []
+for x in file:
+    if x == f"2022-08-{date_search}.expense_data.json":
+        matches.append(x)
+print(matches)
 
-try:
-    with open("ec_app_data/user_settings.json", "r") as file:
-        jsonData = json.load(file)
-except FileNotFoundError:
-    print("Sorry, no file")
+
+# ToDo: need to match files in given directory to date range specified by user;
