abdbe3ff1b0026e044acf47d85a3e457f88897dc
Dan Garro <dobetterwithdan@gmail.com>
2022-08-16 18:00:15 -0400

    testing how to retrieve files/data to generate report

--- test.py
+++ test.py
@@ -2,11 +2,45 @@
 import json
 import re
 from pathlib import Path
+import os
 
+path = "./ec_expenses"
+path1 = "./ec_sales"
 
-start_date = "2022-08-10"
-end_date = "2022-08-14"
+files_exp = [f for f in os.listdir(path)]
 
+files_sale = [f for f in os.listdir(path1)]
+# print(files_exp)
+# print(files_sale)
+
+# x = files_exp[0]
+
+# print(x[8:10])
+start_date = "2022-08-13"
+end_date = "2022-08-16"
+
+y_start = start_date[:4]
+m_start = start_date[5:7]
+d_start = start_date[8:]
+y_end = end_date[:4]
+m_end = end_date[5:7]
+d_end = end_date[8:]
+files_in_range = []
+if y_start == y_end:
+    if m_start == m_end:
+        date_range = range(int(d_start), int(d_end) + 1)
+for file in files_exp:
+    if file[8:10] >= d_start and file[8:10] <= d_end:
+        files_in_range.append(file)
+print(files_in_range)
+
+for i in files_in_range:
+    with open(f"ec_expenses/{i}", "r") as file:
+        fileData = json.load(file)
+        for x in fileData.keys():
+            print(x)
+
+"""
 y_start = start_date[:4]
 m_start = start_date[5:7]
 d_start = start_date[8:]
@@ -25,3 +59,4 @@ print(matches)
 
 
 # ToDo: need to match files in given directory to date range specified by user;
+"""
